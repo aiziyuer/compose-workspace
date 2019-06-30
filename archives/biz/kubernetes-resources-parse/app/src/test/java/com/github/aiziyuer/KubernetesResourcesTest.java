93095a40a26c9b6bc33776ae3516bb6c08176a88
aiziyuer <ziyu01234567789@gmail.com>
2019-06-30 20:16:51 +0800

    归档一个可用的deployment

---
+++ archives/biz/kubernetes-resources-parse/app/src/test/java/com/github/aiziyuer/KubernetesResourcesTest.java
@@ -0,0 +1,19 @@
+package com.github.aiziyuer;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class KubernetesResourcesTest {
+	
+    @Test
+    public void test001() {
+    	log.info("test001");
+    }
+    
+    
+    
+    
+
+}
