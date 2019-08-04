3ad2580a8509c98159e1cc951dfd509e0dfed023
aiziyuer <moss.licong@huawei.cm>
2019-08-04 13:21:11 -0400

    优化代码

--- archives/biz/luarocks-reserve-proxy/conf/openresty/lua-scripts/access.lua
+++ archives/biz/luarocks-reserve-proxy/conf/openresty/lua-scripts/access.lua
@@ -1,10 +1,9 @@
 
-local ngx = ngx
 local _M  = {}
 
 function _M.go()
 
-    
+    require("mobdebug").start("192.168.50.135")
 
     -- 根据访问的host:port来查询对应的后端服务
     local key = "backend:"..ngx.var.http_host..":"..ngx.var.server_port
