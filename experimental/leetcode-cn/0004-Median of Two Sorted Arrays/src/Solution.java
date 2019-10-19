0db54e96eb5f409e1c48b0645ad5150ad52f058e
aiziyuer <ziyu01234567789@gmail.com>
2019-10-19 16:52:49 +0800

    归档原题

---
+++ experimental/leetcode-cn/0004-Median of Two Sorted Arrays/src/Solution.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * 
+ * Find the median of the two sorted arrays. The overall run time complexity
+ * should be O(log (m+n)).
+ * 
+ * You may assume nums1 and nums2 cannot be both empty.
+ * 
+ * Example 1:
+ * 
+ * nums1 = [1, 3] nums2 = [2]
+ * 
+ * The median is 2.0
+ * 
+ * Example 2:
+ * 
+ * nums1 = [1, 2] nums2 = [3, 4]
+ * 
+ * The median is (2 + 3)/2 = 2.5
+ * 
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * 
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * 
+ * 示例 1:
+ * 
+ * nums1 = [1, 3] nums2 = [2]
+ * 
+ * 则中位数是 2.0
+ * 
+ * 示例 2:
+ * 
+ * nums1 = [1, 2] nums2 = [3, 4]
+ * 
+ * 则中位数是 (2 + 3)/2 = 2.5
+ * 
+ */
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    	
+        
+    	return -1;
+    }
+}
---
+++ experimental/leetcode-cn/0004-Median of Two Sorted Arrays/src/SolutionTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+	@Test
+	void test() {
+		//
+		assertEquals(3, new Solution().findMedianSortedArrays(null, null));
+	}
+
+}
