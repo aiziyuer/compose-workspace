7dee5b6617a05e21044906288e16ca00a0b9334b
root <root@localhost.localdomain>
2019-12-04 12:18:48 -0500

    basic任务支持安装必要软件

--- experimental/kubernetes-ansible/plugins/filter/split.py
+++ experimental/kubernetes-ansible/plugins/filter/split.py
@@ -1,22 +1,28 @@
 from ansible import errors
 import re
 
+
 def split_string(string, separator=' '):
     try:
         return string.split(separator)
     except Exception as e:
-        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % str(e),str(string) )
+        raise errors.AnsibleFilterError(
+            'split plugin error: %s, provided string: "%s"' % str(e), str(string))
 
-def split_regex(string, separator_pattern='\s+'):
+
+def split_regex(string, separator_pattern=r'\s+'):
     try:
         return re.split(separator_pattern, string)
     except Exception as e:
-        raise errors.AnsibleFilterError('split plugin error: %s, provided string: "%s"' % str(e),str(string) )
+        raise errors.AnsibleFilterError(
+            'split plugin error: %s, provided string: "%s"' % str(e), str(string))
+
 
 class FilterModule(object):
     ''' A filter to split a string into a list. '''
+
     def filters(self):
         return {
-            'split' : split_string,
-            'split_regex' : split_regex,
+            'split': split_string,
+            'split_regex': split_regex,
         }
