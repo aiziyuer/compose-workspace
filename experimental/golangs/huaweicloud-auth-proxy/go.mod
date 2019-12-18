06ce46da60662bf80f71af37505ece15d166f897
root <root@localhost.localdomain>
2019-12-18 13:29:39 -0500

    简化工程

--- experimental/golangs/huaweicloud-auth-proxy/go.mod
+++
@@ -1,5 +0,0 @@
-module huaweicloud-proxy
-
-go 1.12
-
-require github.com/gophercloud/gophercloud v0.7.0
--- experimental/golangs/huaweicloud-auth-proxy/src/huaweicloud-proxy/server.go
+++ experimental/golangs/huaweicloud-auth-proxy/src/huaweicloud-proxy/server.go
@@ -2,62 +2,20 @@
 package main
 
 import (
-	"github.com/gophercloud/gophercloud/auth/aksk"
-	"github.com/gophercloud/gophercloud"
-	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
-	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/huaweicse/iam"
 	"fmt"
 )
 
-
 func main() {
-	//设置认证参数
-	akskOpts := aksk.AKSKOptions{
-		IdentityEndpoint: "https://iam.example.com/v3",
-		DomainID:         "{domainid}",
-		ProjectID:        "{projectid}",
-		Cloud:            "myhuaweicloud.com",
-		Region:           "cn-north-1",
-		AccessKey:        "{your AK string}",
-		SecretKey:        "{your SK string}",
-	}
-	//初始化provider client
-	provider, providerErr := openstack.AuthenticatedClient(akskOpts)
-	if providerErr != nil {
-		fmt.Println("init provider client error:", providerErr)
-		panic(providerErr)
-	}
+	
+	var c *iam.Client
+	var err error
+	c, err = iam.NewClient(iam.Options{
+		Endpoint:  "https://iam.cn-north-1.myhuaweicloud.com",
+		AccessKey: "xxx",
+		SecretKey: "xxx",
+		Project:   "default",
+	})
 
-	//初始化service client
-	sc, serviceErr := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{})
-	if serviceErr != nil {
-		fmt.Println("init compute service client error:", serviceErr)
-		panic(serviceErr)
-	}
-
-	//列出所有服务器
-	allPages, err := servers.List(sc, servers.ListOpts{}).AllPages()
-
-	if err != nil {
-		fmt.Println("request server list error:", err)
-		panic(err)
-	}
-	//解析返回值
-	allServers, err := servers.ExtractServers(allPages)
-	if err != nil {
-		fmt.Println("extract response data error:", err)
-		if ue, ok := err.(*gophercloud.UnifiedError); ok {
-			fmt.Println("ErrCode:", ue.ErrorCode())
-			fmt.Println("Message:", ue.Message())
-		}
-		return
-	}
-	//打印信息
-	fmt.Println("List Servers:")
-	for _, s := range allServers {
-		fmt.Println("server ID is :", s.ID)
-		fmt.Println("server name is :", s.Name)
-		fmt.Println("server Status is :", s.Status)
-		fmt.Println("server AvailbiltyZone is :", s.AvailbiltyZone)
-	}
+	fmt.Println("hello world", c, err)
 }
