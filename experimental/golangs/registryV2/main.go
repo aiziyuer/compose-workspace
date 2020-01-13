ce73b183b1300b381758150da5373b46728b7a8f
aiziyuer <ziyu0123456789@gmail.com>
2020-01-14 00:46:54 +0800

    启动时默认加载.env配置

--- experimental/golangs/registryV2/main.go
+++ experimental/golangs/registryV2/main.go
@@ -4,7 +4,14 @@ Copyright © 2020 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/aiziyuer/registryV2/cmd"
+import (
+	"github.com/aiziyuer/registryV2/cmd"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	_ = godotenv.Load()
+}
 
 func main() {
 	cmd.Execute()
