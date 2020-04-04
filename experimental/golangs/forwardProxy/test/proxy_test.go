ce8d9a80a3bf5f11bdfab0aa707b66b0d7d9ce6f
aiziyuer <ziyu0123456789@gmail.com>
2020-04-05 01:38:44 +0800

    实现以socks/http为upstream的socks代理

--- experimental/golangs/forwardProxy/test/proxy_test.go
+++ experimental/golangs/forwardProxy/test/proxy_test.go
@@ -1,7 +1,9 @@
 package test
 
 import (
+	"github.com/armon/go-socks5"
 	"github.com/mwitkow/go-http-dialer"
+	"golang.org/x/net/context"
 	"golang.org/x/net/proxy"
 	"gopkg.in/elazarl/goproxy.v1"
 	"log"
@@ -24,6 +26,7 @@ func TestHttpProxy2HttpUpstream(t *testing.T) {
 		return d.Dial(network, addr)
 	}
 
+	// curl -vI -x 127.0.0.1:8080 https://www.google.com
 	log.Fatal(http.ListenAndServe(":8080", httpProxy))
 }
 
@@ -39,5 +42,39 @@ func TestHttpProxy2SocketUpstream(t *testing.T) {
 		return d.Dial(network, addr)
 	}
 
+	// curl -vI -x 127.0.0.1:8080 https://www.google.com
 	log.Fatal(http.ListenAndServe(":8080", httpProxy))
+}
+
+func TestSocketProxy2HttpUpstream(t *testing.T) {
+
+	conf := &socks5.Config{
+		Dial: func(ctx context.Context, network, addr string) (conn net.Conn, err error) {
+
+			proxyUrl, _ := url.Parse("http://10.10.10.254:3128")
+			d := http_dialer.New(proxyUrl)
+
+			return d.Dial(network, addr)
+		},
+	}
+	server, _ := socks5.New(conf)
+
+	// curl -vI -x socks5h://127.0.0.1:8080 https://www.google.com
+	log.Fatal(server.ListenAndServe("tcp", ":8080"))
+}
+
+func TestSocketProxy2SocketUpstream(t *testing.T) {
+
+	conf := &socks5.Config{
+		Dial: func(ctx context.Context, network, addr string) (conn net.Conn, err error) {
+
+			d, _ := proxy.SOCKS5("tcp", "10.10.10.254:1080", nil, proxy.Direct)
+
+			return d.Dial(network, addr)
+		},
+	}
+	server, _ := socks5.New(conf)
+
+	// curl -vI -x socks5h://127.0.0.1:8080 https://www.google.com
+	log.Fatal(server.ListenAndServe("tcp", ":8080"))
 }
